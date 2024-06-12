86758b7f7c0451c2b2e9311509209a29170a9c45
AngieRecalde <166680042+AngieRecalde@users.noreply.github.com>
2024-06-11 22:42:15 -0500

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 from utils.db import db
 from services.estudiante_routes import estudiante_routes
 #from services.especialistas import especialistas
 #from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION
+def crear_app():
+    app = Flask(__name__)
+    # Configuración de la base de datos
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///default.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # SQLAlchemy(app)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SQLAlchemy(app)
-
-db.init_app(app) 
-app.register_blueprint(estudiante_routes)
-#app.register_blueprint(especialistas)
-
-with app.app_context():  # se ejecuta si la clase contacts no existiera
-    db.create_all()
+    db.init_app(app) 
+    app.register_blueprint(estudiante_routes)
+    with app.app_context():  # se ejecuta si la clase contacts no existiera
+        db.create_all()
+    return app
 
 if __name__ == '__main__':
+    app = crear_app()
     app.run(host='0.0.0.0', debug=True, port=5000)

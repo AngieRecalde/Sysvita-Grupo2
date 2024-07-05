251941cd2322a475aaa9184a6e906699b2906677
Alex020601 <68959681+Alex020601@users.noreply.github.com>
2024-07-05 10:09:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
 
+# Cargar variables de entorno del archivo .env si existe
 load_dotenv()
 
-user=os.environ['USER']
-pwd=os.environ['PASSWORD']
-host=os.environ['HOST']
-database=os.environ['DATABASE']
-server=os.environ['SERVER']
+# Usar os.environ.get() con valores por defecto para mayor seguridad
+user = os.environ.get('DB_USER', 'default_user')
+pwd = os.environ.get('DB_PASSWORD', 'default_password')
+host = os.environ.get('DB_HOST', 'default_host')
+database = os.environ.get('DB_NAME', 'default_database')
+server = os.environ.get('DB_SERVER', 'postgresql')
 
-DATABASE_CONNECTION=f'{server}://{user}:{pwd}@{host}/{database}'
+DATABASE_CONNECTION = f'{server}://{user}:{pwd}@{host}/{database}'
